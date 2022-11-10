a21f5fcc754264400ae73fc121acfb9db52bd5ba
Cristhian Palacios <m11mario11m@gmail.com>
2022-11-10 14:17:28 -0500

    create project

---
+++ src/main/java/com/cristhian/springboot/webflux/webclient/app/SpringWebfluxMoviesTrainApplication.java
@@ -0,0 +1,13 @@
+package com.cristhian.springboot.webflux.webclient.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebfluxMoviesTrainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebfluxMoviesTrainApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cristhian/springboot/webflux/webclient/app/SpringWebfluxMoviesTrainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cristhian.springboot.webflux.webclient.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebfluxMoviesTrainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
